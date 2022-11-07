74481f19846319c0cf984f8912d254dc4e767cdd
aminulmoon <aminulislammoon@gmail.com>
2022-11-07 19:07:25 +0600

    Get Amazon Product Data

--- Module-23 Amazon Part-1.py
+++ Module-23 Amazon Part-1.py
@@ -1,8 +1,22 @@
 import os
 from dotenv import load_dotenv
+from amazon_paapi import AmazonApi
+
 load_dotenv()
 
-wp_user = os.getenv('wp_user')
-wp_password = os.getenv('wp_password')
+KEY = os.getenv('KEY')
+SECRET = os.getenv('SECRET')
+TAG = os.getenv('TAG')
+COUNTRY = os.getenv('COUNTRY')
+amazon = AmazonApi(KEY, SECRET, TAG, COUNTRY)
+item = amazon.get_items('B000JFJLP6')[0]
+search_result = amazon.search_items(keywords='desktop mouse')
 
-print(wp_user, wp_password)
+for item in search_result.items:
+    title = item.item_info.title.display_value
+    image = item.images.primary.large.url
+    product_description = item.item_info.features.display_values
+    product_link = item.detail_page_url
+
+
+print(product_link)
