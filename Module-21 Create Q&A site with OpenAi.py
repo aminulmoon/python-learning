09984d79a49fda1ea9aa49679051e57b7d5dcaa4
aminulmoon <aminulislammoon@gmail.com>
2022-11-08 03:53:01 +0600

    Get Q&A from OpenAi

--- Module-21 Create Q&A site with OpenAi.py
+++ Module-21 Create Q&A site with OpenAi.py
@@ -1,8 +1,45 @@
-import httpx
+# import httpx
+#
+# url_list = ['https://www.google.com', 'https://www.google.com/fgsag', 'https://google.com']
+#
+# for url in url_list:
+#     res = httpx.get(url)
+#     status = res.status_code
+#     print(url, res, sep='.....')
 
-url_list = ['https://www.google.com', 'https://www.google.com/fgsag', 'https://google.com']
 
-for url in url_list:
-    res = httpx.get(url)
-    status = res.status_code
-    print(url, res, sep='.....')
+
+import openai
+
+openai.api_key = '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'
+
+def openai_ans(my_questions):
+  response = openai.Completion.create(
+    model="text-davinci-002",
+    prompt= my_questions,
+    temperature=0.7,
+    max_tokens=321,
+    top_p=1,
+    frequency_penalty=0,
+    presence_penalty=0
+  )
+  data = response.get('choices')[0].get('text').strip('\n')
+  return data
+
+data = openai_ans('write 2 questinos about python')
+number_list = ['1. ', '2. ', '3. ', '4. ', '5. ']
+
+for number in number_list:
+  data = data.replace(number, '')
+
+questions = data.split('\n')
+
+while '' in questions:
+  questions.remove('')
+
+questions_ans = {}
+for q in questions:
+  answer = openai_ans(q)
+  questions_ans[q] = answer
+
+print(questions_ans)
